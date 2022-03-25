17320be0c4b298552cb18bfd2e3704a303255ae6
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-03-25 15:00:46 +0530

    contest

---
+++ codechef/problems/contest.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#define el "\n"
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+typedef long long ll;
+// typedef vector<int> vi;
+using namespace std;
+
+//---------------------- Solution Starts Here ----------------------
+
+void solve() {
+  cout << 2 << el; 
+}
+
+//---------------------- Solution Ends Here ----------------------
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
