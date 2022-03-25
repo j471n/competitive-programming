eca63e4503b83a90a39be44a27b0585aa6d4bc8f
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-03-25 15:32:02 +0530

    contest

--- codechef/problems/contest.cpp
+++ codechef/problems/contest.cpp
@@ -10,25 +10,34 @@ using namespace std;
 //---------------------- Solution Starts Here ----------------------
 
 void solve() {
-  int x, y, a, b;
-  cin >> x >> y >> a >> b ;
+  int a, b, c, d, sum, x,y, sub, mul, div;
+  cin >> a >> b >> c >> d;
 
-  int win = 2;
 
-  // if((x==a) || (x==b) || (y==a) || (y==b)){
-  //   cout << 0 << el;
-  // } 
+  for(int i=0; i<10000; ++i){
+    x= i;
+    y=i+1;
 
-  if(((x==a) || (x==b)) && ((y==b) || (y==a))){
-    win -= 2;
+    sum = x+y;
+    sub = x-y;
+    mul = x*y;
+    div = abs(x/y);
+
+    bool sumCheck =  (sum==a) || (sum ==b) || (sum==c) || (sum==d);
+    bool subCheck =  (sub==a) || (sub ==b) || (sub==c) || (sub==d);
+    bool mulCheck =  (mul==a) || (mul ==b) || (mul==c) || (mul==d);
+    bool divCheck =  (div==a) || (div ==b) || (div==c) || (div==d);
+
+    if(sumCheck && subCheck && mulCheck && divCheck){
+      cout << x << " " << y << el;
+      exit(0);
+    }
+
   }
-  else if ((x==a) || (x==b)){
-    win--;
-  } else if((y==b) || (y==a)){
-    win--;
-  }
 
-  cout << win << el;
+  cout << -1 << " " << -1 << el;
+
+
 
 }
 
