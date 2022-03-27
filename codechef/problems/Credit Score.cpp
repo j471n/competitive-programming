f3076086e53eab907e7d900b44ac390b5aec68a1
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-03-27 16:19:57 +0530

    Credit Score

---
+++ codechef/problems/Credit Score.cpp
@@ -0,0 +1,11 @@
+// Problem Code : CREDSCORE
+// https://www.codechef.com/problems/CREDSCORE
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x;
+  cin >> x;
+  (x >= 750) ? cout << "yes" : cout << "no";
+}
