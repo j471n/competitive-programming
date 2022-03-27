386591f093a8f43ded8166727eb9eca160407862
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-03-27 16:36:38 +0530

    Update default.cpp

--- default.cpp
+++ default.cpp
@@ -2,7 +2,9 @@
 
 #define el "\n"
 #define rep(i, a, b) for (int i = a; i < b; i++)
-typedef long long ll;
+#define ll long long
+#define ld long double
+
 using namespace std;
 
 //---------------------- Solution Starts Here ----------------------
@@ -10,6 +12,7 @@ using namespace std;
 void solve() {}
 
 //---------------------- Solution Ends Here ----------------------
+
 int main() {
   int t;
   cin >> t;
