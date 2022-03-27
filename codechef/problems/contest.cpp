63d2293d41b04cd81e8eb78cef4fcdb64e167b4d
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-03-27 16:34:36 +0530

    contest

--- codechef/problems/contest.cpp
+++ codechef/problems/contest.cpp
@@ -2,22 +2,30 @@
 
 #define el "\n"
 #define rep(i, a, b) for (int i = a; i < b; i++)
-
-typedef long long ll;
-// typedef vector<int> vi;
+#define ll long long
+#define ld long double
+#define print(value) cout << value << el
+#define printA(arr)                                        \
+  for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) { \
+    cout << i << " ";                                      \
+  }
+#define scanA(arr, len) \
+  rep(i, 0, len) { cin >> arr[i]; }
 using namespace std;
 
 //---------------------- Solution Starts Here ----------------------
 
 void solve() {
-  int s;
-  cin >> s;
-  int x=1, y;
-  y = -x-s;
+  int x, y;
+  cin >> x >> y;
 
-  cout << y << el;
+  int credCoins = x * y;
+  if (credCoins >= 100) {
+    cout << credCoins / 100 << el;
+  } else {
+    cout << 0 << el;
+  }
 }
-
 //---------------------- Solution Ends Here ----------------------
 
 int main() {
